487f1fde25321f0f7efbafb345b1e1b7f67d12a4
Aaron Hill <aa1ronham@gmail.com>
2015-01-27 11:33:38 -0500

    Expand test

--- src/test/java/org/aaron1011/whowas/core/PlayerNameHistoryFetcherTest.java
+++ src/test/java/org/aaron1011/whowas/core/PlayerNameHistoryFetcherTest.java
@@ -1,5 +1,6 @@
 package org.aaron1011.whowas.core;
 
+import com.google.common.base.Optional;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import static org.junit.Assert.*;
@@ -7,6 +8,8 @@ import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
 
 import java.io.ByteArrayInputStream;
+import java.util.Date;
+import java.util.List;
 import java.util.UUID;
 
 import static org.powermock.api.mockito.PowerMockito.*;
@@ -24,6 +27,21 @@ public class PlayerNameHistoryFetcherTest {
         UUID uuid = UUID.randomUUID();
 
         PlayerNameHistory history = PlayerNameHistoryFetcher.getPlayerNameHistory(uuid);
+        List<TimestampedName> names = history.getNames();
+
         assertEquals("UUIDS are not equal", uuid, history.getUuid());
+
+        TimestampedName first = names.get(0);
+        TimestampedName second = names.get(1);
+        TimestampedName third = names.get(2);
+
+        assertEquals("Djinnibone", first.getName());
+        assertEquals(Optional.absent(), first.getChangedToAt());
+
+        assertEquals("Djinnibutt", second.getName());
+        assertEquals(Optional.fromNullable(new Date(1413217187000L)), second.getChangedToAt());
+
+        assertEquals("Djinnibone", third.getName());
+        assertEquals(Optional.fromNullable(new Date(1413217202000L)), third.getChangedToAt());
     }
 }
