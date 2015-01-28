0848585da0b0600dc5009849440e5c003d33887b
Aaron Hill <aa1ronham@gmail.com>
2015-01-28 07:20:38 -0500

    Add artifact uploading

---
+++ upload_artifact.py
@@ -0,0 +1,21 @@
+from os import environ as env
+from glob import glob
+
+import datetime
+import dropbox
+
+FILENAME = 'build/libs/WhoWas.jar'
+
+def main():
+    client = dropbox.client.DropboxClient(env.get('DROPBOX_ACCESS_TOKEN'))
+    base = filename.split('/')[2].split('.jar')[0]
+    date = datetime.datetime.now().strftime("%Y-%m-%d")
+    build_id = env['TRAVIS_JOB_NUMBER']
+
+    with open(filename) as f:
+        client.put_file('/' + base + '-' + date + '-' + build_id, f, overwrite=True)
+    print("Successfully uploaded latest build to Dropbox")
+
+
+if __name__ == "__main__":
+    main()
