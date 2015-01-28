c6b6c82d99e19638abe10770b8688d8a1d0c300d
Aaron Hill <aa1ronham@gmail.com>
2015-01-28 06:56:18 -0500

    Install Metrics

--- src/main/java/org/aaron1011/whowas/impl/bukkit/BukkitPlugin.java
+++ src/main/java/org/aaron1011/whowas/impl/bukkit/BukkitPlugin.java
@@ -2,11 +2,20 @@ package org.aaron1011.whowas.impl.bukkit;
 
 import org.aaron1011.whowas.impl.bukkit.command.WhoWasCommand;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.mcstats.Metrics;
+
+import java.io.IOException;
 
 public class BukkitPlugin extends JavaPlugin {
 
     @Override
     public void onEnable() {
+        try {
+            Metrics metrics = new Metrics(this);
+            metrics.start();
+        } catch (IOException e) {
+            // Failed to submit the stats :-(
+        }
         getCommand("whoWas").setExecutor(new WhoWasCommand());
     }
 }
