2eacd967e61a001c22c5fcf45183b7eaac4a1deb
Aaron Hill <aa1ronham@gmail.com>
2015-01-28 21:37:41 -0500

    Enable auto-updater

--- src/main/java/org/aaron1011/whowas/impl/bukkit/BukkitPlugin.java
+++ src/main/java/org/aaron1011/whowas/impl/bukkit/BukkitPlugin.java
@@ -19,27 +19,27 @@ public class BukkitPlugin extends JavaPlugin {
         }
         getCommand("whoWas").setExecutor(new WhoWasCommand());
         // Waiting for plugin id
-        //runUpdater();
+        runUpdater();
     }
 
     private void runUpdater() {
         if (getConfig().contains("update.checkForUpdates") && getConfig().getBoolean("update.checkForUpdates")) {
             getLogger().info("Checking for updates");
             Updater.UpdateType type = getConfig().getBoolean("update.autoInstallUpdates") ? Updater.UpdateType.DEFAULT : Updater.UpdateType.NO_DOWNLOAD;
-            Updater updater = new Updater(this, 81330, getFile(), type, true);
+            Updater updater = new Updater(this, 88955, getFile(), type, true);
             switch (updater.getResult()) {
                 case FAIL_BADID:
                 case FAIL_NOVERSION:
                     getLogger().severe("Failed to check for updates due to a bad plugin name/id. Contact the developer: " + updater.getResult().name());
                     break;
                 case FAIL_DBO:
-                    getLogger().severe("An error occured while checking Bukki Dev for updates");
+                    getLogger().severe("An error occured while checking Bukkit Dev for updates");
                     break;
                 case FAIL_DOWNLOAD:
                     getLogger().severe("An error occurred downloading the update.");
                     break;
                 case UPDATE_AVAILABLE:
-                    getLogger().warning("An update for InventoryBomb is available to download on Bukki Dev");
+                    getLogger().warning("An update for InventoryBomb is available to download on Bukkit Dev");
                     break;
                 case NO_UPDATE:
                     getLogger().info("InventoryBomb is up to date!");
